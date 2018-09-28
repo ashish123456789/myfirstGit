711144bac75fd9663e7cdcfbb2cac3d36b9af3f2
ashish <ashish.nain@mobilous.com>
2018-09-28 14:29:16 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,1 +1,18 @@
-Hello Ashis
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from main thread");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        new Thread(){
+            @Override
+            public void run() {
+                System.out.println("hello from anonymous thread");
+            }
+        }.start();
+        System.out.println("hello again main thread");
+    }
+}
+
